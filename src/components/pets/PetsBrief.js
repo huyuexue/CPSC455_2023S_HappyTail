455d39e7f6234449fd4e691a41136b567cb9f359
James Ross <james.rosstwo@gmail.com>
2023-06-08 20:38:06 -0400

    separate petcard into a new component

--- src/components/pets/PetsBrief.js
+++ src/components/pets/PetsBrief.js
@@ -1,27 +1,17 @@
 import {useDispatch, useSelector} from "react-redux";
 import {petsState} from "./petsSlice";
-import {openDetailView, selectPet} from "../petDetail/petDetailSlice";
+import PetCard from "./PetCard";
 
 export default function PetsBrief() {
     const petsCurState = useSelector(petsState);
     const pets = petsCurState.list;
-    const dispatch = useDispatch();
 
     return(
         <div>
             <h2>Pets Looking for A Forever Home</h2>
             <div className="petList" >
                 {pets.map((pet) => (
-                    <div className="pet" key={pet.id}
-                         onClick={() => {
-                             dispatch(selectPet(pet));
-                             dispatch(openDetailView());
-                         }}>
-                        <div className="imageContainer">
-                            <img src={pet.pictureUrl} alt="Not available" />
-                        </div>
-                        <h3>{pet.name}</h3>
-                    </div>
+                    <PetCard pet={pet}></PetCard>
                 ))}
             </div>
 
