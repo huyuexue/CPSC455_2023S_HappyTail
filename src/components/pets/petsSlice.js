0387141a8abe0ce8b595b8f0f77e62808e7b9fbb
Dasburger69 <ayandas99@gmail.com>
2023-06-10 00:35:09 -0700

    added slideshow

--- src/components/pets/petsSlice.js
+++ src/components/pets/petsSlice.js
@@ -11,6 +11,26 @@ const initialState  = {
             pictureUrl: "https://www.thesprucepets.com/thmb/AUhVkJo7GEsadFm4jevINielAuQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/black-lab-names-5093386-hero-5d14645833114fda9f7e3e620e5e4d67.jpg",
             description : "A happy girl."
         },
+        {id:1,
+            name: "Lola",
+            species: 'dog',
+            breed: "Lab",
+            gender: "Girl",
+            age: 5,
+            pictureUrl: "https://images.unsplash.com/photo-1587559045816-8b0a54d1fbd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmVtYWxlJTIwZG9nfGVufDB8fDB8fHww&w=1000&q=80",
+            description : "A happy pet."
+
+        },
+        {id:2,
+            name: "Sal",
+            species: 'dog',
+            breed: "Lab",
+            gender: "Girl",
+            age: 5,
+            pictureUrl: "https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+            description : "A happy pet."
+
+        }
         ],
     sort: 1,
     search: "show all"
