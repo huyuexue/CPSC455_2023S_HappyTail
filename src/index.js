40abe501a56793a2d2d26b1a976562020b0231d0
Ethan Nguyen <gsthanhnguyen@gmail.com>
2023-06-10 03:22:39 -0700

    initial push

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./store";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <Router>
-              <Routes>
-                  <Route path = "/*" element={<App />} />
-              </Routes>
-          </Router>
-      </Provider>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
