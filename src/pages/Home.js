0b2fafdaa13fd878c71e73be812050477b288fde
huyuexue <70684186+huyuexue@users.noreply.github.com>
2023-06-19 20:23:57 -0700

    merge homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import AddNewPet from "../components/forms/AddNewPet";
+//import AddNewPet from "../components/forms/AddNewPet";
 import PetsBrief from "../components/pets/PetsBrief";
 import PetDetail from "../components/petDetail/PetDetail";
 import {useSelector} from "react-redux";
@@ -8,7 +8,7 @@ export default function Home({itemsList}){
     const detailViewIsOpen = useSelector(detailViewStatus)
     return (
         <div className="Home">
-            <AddNewPet />
+            {/*<AddNewPet />*/}
             <ButtonBar />
             <PetsBrief />
             {detailViewIsOpen && <PetDetail />}
