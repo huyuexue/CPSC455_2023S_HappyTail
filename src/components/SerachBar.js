8ef61bd000bc10b5236e158eeb4918d030e26b54
huyuexue <70684186+huyuexue@users.noreply.github.com>
2023-06-19 19:40:16 -0700

    Tina:homepage + upload form

---
+++ src/components/SerachBar.js
@@ -0,0 +1,40 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+export default function SearchBar() {
+    const navigate = useNavigate();
+
+    const[searchArea, setSearchArea] = useState("Search By Postal Code");
+    const onSearchFocus = () => {
+        setSearchArea('');
+    }
+
+    const onSearchChange = (e) =>{
+        setSearchArea(e.target.value);
+    }
+
+    const handleBlur = (e) => {
+        if (e.target.value.trim() === ''){
+            setSearchArea("Search By Postal Code");
+        }
+    }
+
+    const onSearchCLicked = () => {
+        navigate('/result');
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                name = "searchArea"
+                value ={searchArea}
+                onChange = {onSearchChange}
+                onFocus = {onSearchFocus}
+                onBlur={handleBlur}
+
+            />
+            <button name="searchButton" onClick={onSearchCLicked}>Search</button>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
-import AddNewPet from "../components/forms/AddNewPet";
+import AddNewPet from "../components/forms/addPetForm/AddNewPet";
 import PetsBrief from "../components/pets/PetsBrief";
 import PetDetail from "../components/petDetail/PetDetail";
 import {useSelector} from "react-redux";
 import {detailViewStatus} from "../components/petDetail/petDetailSlice";
+import SearchBar from "../components/SerachBar";
+import RehomeButton from "../components/RehomeButton";
 
 export default function Home({itemsList}){
     const detailViewIsOpen = useSelector(detailViewStatus)
     return (
         <div className="Home">
-            <AddNewPet />
+            <SearchBar />
+            <RehomeButton />
             <PetsBrief />
             {detailViewIsOpen && <PetDetail />}
         </div>
