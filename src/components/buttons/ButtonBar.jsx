777f1d9be9c75066f8ac6e768e4ff16b10e4bdcc
huyuexue <70684186+huyuexue@users.noreply.github.com>
2023-06-22 15:00:55 -0700

    homepage

--- src/components/buttons/ButtonBar.jsx
+++ src/components/buttons/ButtonBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MuiButton from './MuiButton';
 import PostCodeSearchBar from '../forms/PostCodeSearchBar';
 import { Stack, Box } from "@mui/material";
+import RehomeButton from "../RehomeButton";
 
 export default function ButtonBar() {
     return (
@@ -15,6 +16,10 @@ export default function ButtonBar() {
                     <MuiButton variant="text" color="primary" link='/result' imageSrc='https://cdn.pixabay.com/photo/2020/02/12/05/05/dog-cartoon-4841690_1280.jpg'>
                         Dogs
                     </MuiButton>
+                    <div className="vl"></div>
+                    <MuiButton  variant="text" color="primary" link='/addNewPet' imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvNrnjrIw6at92tYPeoB3Fn_fUj6PNniE_3w&usqp=CAU">
+                    </MuiButton>
+
                 </Stack>
             </Stack>
         </Box>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-//import AddNewPet from "../components/forms/AddNewPet";
 import PetsBrief from "../components/pets/PetsBrief";
 import PetDetail from "../components/petDetail/PetDetail";
 import {useSelector} from "react-redux";
@@ -8,7 +7,7 @@ export default function Home({itemsList}){
     const detailViewIsOpen = useSelector(detailViewStatus)
     return (
         <div className="Home">
-            {/*<AddNewPet />*/}
+
             <ButtonBar />
             <PetsBrief />
             {detailViewIsOpen && <PetDetail />}
