7735bd38faf08da821027b6658a96ea886749db1
Ayan Das <ayandas99@gmail.com>
2023-07-24 20:06:54 -0400

    Update imageUpload.js

--- app/src/components/forms/imageUpload/imageUpload.js
+++ app/src/components/forms/imageUpload/imageUpload.js
@@ -3,8 +3,8 @@ import AWS from 'aws-sdk';
 
 //const s3 = new AWS.S3();
 const s3 = new AWS.S3({
-    accessKeyId: 'AKIASXNJ4N6WP6UPL4OP',
-    secretAccessKey: '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',
+    accessKeyId: 'KEY',
+    secretAccessKey: 'SECKEY',
     region: 'us-east-1',
   });
 const bucketName = 'happytailsimages';
