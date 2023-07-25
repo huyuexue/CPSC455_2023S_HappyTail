e0f784907e04c99edeafbc86cca7811a80f353e7
Dasburger69 <ayandas99@gmail.com>
2023-07-24 20:04:54 -0400

    added image upload component

---
+++ app/src/components/forms/imageUpload/imageUpload.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import AWS from 'aws-sdk';
+
+//const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+    accessKeyId: 'AKIASXNJ4N6WP6UPL4OP',
+    secretAccessKey: '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',
+    region: 'us-east-1',
+  });
+const bucketName = 'happytailsimages';
+
+AWS.config.credentials.refresh((error) => {
+    if (error) {
+      console.error('Error refreshing credentials:', error);
+      return;
+    }
+  
+    // Your S3 upload code here
+  });
+
+async function uploadImageToS3(file) {
+    const fileName = file.name;
+    const fileKey = `images/${fileName}`; 
+    const params = {
+      Bucket: bucketName,
+      Key: fileKey,
+      Body: file,
+      ACL: 'public-read', 
+    };
+    params.ContentType = file.type;
+  
+    try {
+      const data = await s3.upload(params).promise();
+      return data.Location; 
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      throw new Error('Image upload failed');
+    }
+  }
+  
+  export default uploadImageToS3;
+  
---
+++ app/src/components/forms/imageUpload/upload.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import uploadImageToS3 from './imageUpload';
+
+const ImageUpload = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      uploadImageToS3(selectedFile)
+        .then((url) => {
+          //setImageUrl(url);
+          console.log(url);
+        })
+        .catch((error) => {
+          console.error('Error uploading image:', error);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+    </div>
+  );
+};
+
+export default ImageUpload;
