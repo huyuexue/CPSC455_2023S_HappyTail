f846f676717e3bae13c377c37c09b905d1dfb114
wzy236 <w23z26y25@gmail.com>
2023-07-27 20:52:07 -0700

    fix the bug that PetsList.js key in not unique

--- app/src/components/pets/PetsList.js
+++ app/src/components/pets/PetsList.js
@@ -74,7 +74,7 @@ export default function PetsList() {
 
                   {pets.map((pet, index) => (
                     <Grid item xs={4} 
-                      key={pet.id}
+                      key={index}
                       display="flex" justifyContent="center" alignItems="center"
                     >
                       { <PetCard pet={pet} setRefresh={setRefresh} token={token} />}
