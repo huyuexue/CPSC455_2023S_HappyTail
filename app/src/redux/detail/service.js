7aabc4869ceb4c5216c80a84d2791c2a6a9f92c5
huyuexue <70684186+huyuexue@users.noreply.github.com>
2023-07-28 13:56:57 -0700

    deploy

--- app/src/redux/detail/service.js
+++ app/src/redux/detail/service.js
@@ -1,6 +1,6 @@
 
 const getPet = async (id) => {
-    const link = `http://localhost:3001/pets/${id.id}`;
+    const link = `https://ht-backend.onrender.com/pets/${id.id}`;
     const res = await fetch(link, {
         method: 'GET'
     });
