49bd625d2a3b0002c21dc74f4739e7538467e636
James Ross <james.rosstwo@gmail.com>
2023-07-29 12:39:05 -0400

    fix fetch

--- app/src/redux/pets/service.js
+++ app/src/redux/pets/service.js
@@ -1,12 +1,12 @@
 const getPets = async () => {
-    const res = await fetch("0.0.0.0:3001/pets/all", {
+    const res = await fetch("http://0.0.0.0:3001/pets/all", {
         method: 'GET'
     });
     return res.json();
 };
 
 const getSearchResults = async ({searchTerm, sortTerm}) => {
-    const res = await fetch(`0.0.0.0:3001/pets/search?${searchTerm}`, {
+    const res = await fetch(`http://0.0.0.0:3001/pets/search?${searchTerm}`, {
         method: 'GET'
     });
     return res.json();
