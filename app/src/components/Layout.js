702184c0068e54fc2d2057c0990fc3e48e5cdcc5
huyuexue <70684186+huyuexue@users.noreply.github.com>
2023-08-05 09:11:06 -0700

    add login in Layout

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -1,6 +1,32 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import NavBar from "./NavBar";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {useState} from "react";
+import {TurnLogin, TurnLogout} from "../redux/login/reducer";
+import {useDispatch} from "react-redux";
 export default function Layout() {
+    const auth = getAuth();
+    const nav = useNavigate();
+    const dispatch = useDispatch();
+
+    const[token, setToken] = useState('');
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            getToken(user).then(r => dispatch(TurnLogin(token))
+            );
+        } else {
+            localStorage.removeItem('tokenId');
+            dispatch(TurnLogout(token));
+        }
+    });
+    const getToken=async (user)=>{
+        const token= await user.getIdToken();
+        console.log(token);
+        setToken(token);
+    }
+
+
     return (
         <div className="App">
             <NavBar />
